8791917e71f7607489ebfd447a92a64e40703098
Evelyn Coleman <coleve27@gmail.com>
2017-12-21 17:51:10 -0600

    final changes

--- RQTZ/assets/javascript/app.js
+++ RQTZ/assets/javascript/app.js
@@ -207,29 +207,29 @@ $.ajax({
 });
 
 
-// var initiate Javascript SDK//
-// window.fbAsyncInit = function () {
-//     FB.init({
-//         appId: '1968644966792089',
-//         autoLogAppEvents: true,
-//         xfbml: true,
-//         version: 'v2.11'
-//     });
-//
-//     FB.getLoginStatus(function (response) {
-//         if (response.status === 'connected') {
-//             console.log('Logged in.');
-//         }
-//         else {
-//             FB.login();
-//         }
-//     });
-// };
-//
-// (function (d, s, id) {
-//     var js, fjs = d.getElementsByTagName(s)[0];
-//     if (d.getElementById(id)) { return; }
-//     js = d.createElement(s); js.id = id;
-//     js.src = "https://connect.facebook.net/en_US/sdk.js";
-//     fjs.parentNode.insertBefore(js, fjs);
-// }(document, 'script', 'facebook-jssdk'));
+//initiate Javascript SDK//
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '1968644966792089',
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v2.11'
+    });
+
+    FB.getLoginStatus(function (response) {
+        if (response.status === 'connected') {
+            console.log('Logged in.');
+        }
+        else {
+            FB.login();
+        }
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
